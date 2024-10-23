3a638387894ee9b262755c262d08ff87f04b69d1
asnts <annandasants@gmail.com>
2024-10-23 15:50:05 -0300

    add nav.js

---
+++ dark_store/src/comp/nav.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { FaCrow  } from 'react-icons/fa';
+import './nav.css'
+const Nav = () => {
+  return (
+
+    <>
+    <div className='header'> 
+<div className='top_header'>
+  <div className='icon'>
+  <FaCrow />
+  </div>
+
+  <div className='info'>
+    <h3>Dark Store</h3>
+  </div>
+   </div>
+
+
+
+    </div>
+    
+    </>
+    
+  
+   )
+
+}
+
+export default Nav
