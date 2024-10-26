a226885539f1a44664b5c6f3ba58a843618842df
asnts <annandasants@gmail.com>
2024-10-25 22:43:11 -0300

    update div top_header: insert logo and elements

--- dark_store/src/comp/nav.js
+++ dark_store/src/comp/nav.js
@@ -1,35 +1,41 @@
 import React from 'react'
-import { FaCrow  } from 'react-icons/fa';
+import { HiSearchCircle } from "react-icons/hi";
+import { BiSolidLogInCircle } from "react-icons/bi";
+
 import './nav.css'
 const Nav = () => {
   return (
 
     <>
-    <div className='header'> 
-<div className='top_header'>
-  <div className='icon'>
-  <FaCrow />
-  </div>
+      <div className='header'>
+        <div className='top_header'>
+          <div className='logo'>
+            <img src='image/darkstore_logo.png' alt='logo_raven' height='80px' width='80px'></img>
+          </div>
+          <div className='search_box'>
+            <input type='text' value='' placeholder='O que você busca?'></input>
+            <button><HiSearchCircle /> </button>
+          </div>
+          <div className='user'>
+            <div className='icon'></div>
+            <BiSolidLogInCircle />
+          </div>
+          <div className='login_btn'></div>
+          <button>Login</button>
+        </div>
 
-  <div className='info'>
-    <h3>Dark Store</h3>
-  </div>
-   </div>
 
-   <div className='mid_header'>
-    <div className='logo'>
-      <img src='image/raven.png' alt='logo_raven' height='80px' width='80px'></img>
-    </div>
-   </div>
+      </div>
+
+      <div className='mid_header'>
 
 
+      </div>
 
-    </div>
-    
     </>
-    
-  
-   )
+
+
+  )
 
 }
 
