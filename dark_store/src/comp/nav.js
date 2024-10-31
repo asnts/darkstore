1192e78250f2dfe76471ca98a8ac71097fd452c8
asnts <annandasants@gmail.com>
2024-10-30 23:26:50 -0300

    add mid_header and related divs

--- dark_store/src/comp/nav.js
+++ dark_store/src/comp/nav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HiSearchCircle } from "react-icons/hi";
-import { BiSolidLogInCircle } from "react-icons/bi";
+
 
 import './nav.css'
 const Nav = () => {
@@ -13,24 +13,40 @@ const Nav = () => {
             <img src='image/darkstore_logo.png' alt='logo_raven' height='80px' width='80px'></img>
           </div>
           <div className='search_box'>
+
             <input type='text' value='' placeholder='O que você busca?'></input>
             <button><HiSearchCircle /> </button>
           </div>
           <div className='user'>
             <div className='icon'></div>
-            <BiSolidLogInCircle />
           </div>
-          <div className='login_btn'></div>
-          <button>Login</button>
+
+          <div className='login_btn'>
+            <button>Login</button>
+          </div>
+
+
         </div>
 
+        <div className='mid_header'>
+        <div className='mid_inicio'>
+          <button>Início</button>
+        </div>
+        <div className='mid_feminino'>
+          <button>Roupas Femininas</button>
+        </div>
+        <div className='mid_masculino'>
+          <button>Roupas Masculinas</button>
+        </div>
+        <div className='mid_acessorios'>
+          <button>Acessórios</button>
+        </div>
+      </div>
+
 
       </div>
 
-      <div className='mid_header'>
-
-
-      </div>
+    
 
     </>
 
