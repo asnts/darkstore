23906958a507b1cde83aede6696168b7d2cd78f7
asnts <annandasants@gmail.com>
2024-11-07 00:46:15 -0300

    add icons login and cart

--- dark_store/src/comp/nav.js
+++ dark_store/src/comp/nav.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { HiSearchCircle } from "react-icons/hi";
+import { FaUser } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
 
 
 import './nav.css'
@@ -17,36 +19,36 @@ const Nav = () => {
             <input type='text' value='' placeholder='O que você busca?'></input>
             <button><HiSearchCircle /> </button>
           </div>
-          <div className='user'>
-            <div className='icon'></div>
+          <div className='login_btn'>
+            <button><FaUser /></button>
           </div>
 
-          <div className='login_btn'>
-            <button>Login</button>
+          <div className='cart'>
+            <button><FaCartShopping /></button>
           </div>
 
 
         </div>
 
         <div className='mid_header'>
-        <div className='mid_inicio'>
-          <button>Início</button>
+          <div className='mid_inicio'>
+            <button>Início</button>
+          </div>
+          <div className='mid_feminino'>
+            <button>Roupas Femininas</button>
+          </div>
+          <div className='mid_masculino'>
+            <button>Roupas Masculinas</button>
+          </div>
+          <div className='mid_acessorios'>
+            <button>Acessórios</button>
+          </div>
         </div>
-        <div className='mid_feminino'>
-          <button>Roupas Femininas</button>
-        </div>
-        <div className='mid_masculino'>
-          <button>Roupas Masculinas</button>
-        </div>
-        <div className='mid_acessorios'>
-          <button>Acessórios</button>
-        </div>
-      </div>
 
 
       </div>
 
-    
+
 
     </>
 
