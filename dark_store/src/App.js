cf263e2ccc0edf600da20a147c492e671ef3d429
asnts <annandasants@gmail.com>
2024-11-07 00:53:31 -0300

    add route carrousel

--- dark_store/src/App.js
+++ dark_store/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './comp/nav'
+import Carrousel from './comp/carrousel'
 import {BrowserRouter} from 'react-router-dom'
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
     <BrowserRouter>
 
     <Nav />
+    <Carrousel />
+    
     
     </BrowserRouter>
     
