41ea3420160cc8a73cb013be5e77dc7f8e9c317e
asnts <annandasants@gmail.com>
2025-01-02 20:48:02 -0300

    feat: ajuste carrousel

---
+++ dark_store/src/comp/carrousel.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+
+import './carrousel.css';
+const Carrousel = () => {
+    return (
+        <>
+
+
+
+            <div className='carrossel'>
+                <img src='image/tresmulheres.jpg' alt='três mulheres vestidas de preto'></img>
+            </div>
+
+
+
+
+
+        </>
+
+
+
+
+
+
+
+
+    )
+}
+
+
+export default Carrousel
